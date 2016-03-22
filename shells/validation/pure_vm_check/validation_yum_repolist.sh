0076a5b16b27aee561113d3cf0a44df8e0c0659d
jooho <jooho@localhost.localdomain>
2016-03-22 16:00:28 -0400

    validation_yum_repolist.sh

---
+++ shells/validation/pure_vm_check/validation_yum_repolist.sh
@@ -0,0 +1,27 @@
+. ../../ose_config.sh 
+
+yum_repolist="rhel-7-server-extras-rpms rhel-7-server-ose-3.1-rpms rhel-7-server-rpms rhel-ha-for-rhel-7-server-rpms"
+
+export success=0
+export yum_repolist_count=0
+#yum clean all; yum repolist > ./yum_repolist.out
+ yum repolist > ./yum_repolist.out
+for repo in $yum_repolist
+do
+  yum_repolist_count=$((yum_repolist_count + 1))
+  temp_result=$(grep -c "$repo" ./yum_repolist.out)
+  if [[ $? == 0 ]]; then
+     echo "$repo is attached"
+     success=$((success + 1))
+  fi
+done
+
+echo "------------------------------ "
+echo "Needed repolist count :  $yum_repolist_count "
+echo "Attached VM count : $success"
+cat ./yum_repolist.out
+if [[ $yum_repolist_count == $success ]];then
+  echo "PASS !!"
+else
+  echo "FAIL ;("
+fi
