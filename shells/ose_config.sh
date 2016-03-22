cfaf61f401e1dcb68f56c3cbf84aeb2e31fd9a85
jooho lee <ljhiyh@gmail.com>
2016-03-21 21:48:40 -0400

    ose_config.sh

---
+++ shells/ose_config.sh
@@ -0,0 +1,3 @@
+export all_hosts="master1.example.com master2.example.com master3.example.com node1.example.com node2.example.com node3.example.com node4.example.com node5.example.com lb.example.com "
+
+export etcd_is_installed_on_master="true"
