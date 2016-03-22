33766c1e4381f1837b725a6191ed169bb3a88b0e
jooho <jooho@localhost.localdomain>
2016-03-21 22:15:48 -0400

    pure_vm_check dns shell finish

--- shells/ose_config.sh
+++ shells/ose_config.sh
@@ -1,3 +1,4 @@
 export all_hosts="master1.example.com master2.example.com master3.example.com node1.example.com node2.example.com node3.example.com node4.example.com node5.example.com lb.example.com "
+export all_ip="192.168.200.100 192.168.200.101 192.168.200.102 192.168.200.104 192.168.200.105 192.168.200.106 192.168.200.107 192.168.200.108 192.168.200.108"
 
 export etcd_is_installed_on_master="true"
---
+++ shells/validation/pure_vm_check/validation_dns_lookup.sh
@@ -0,0 +1,25 @@
+. ../ose_config.sh 
+
+export success=0
+export all_hosts_count=0
+for host in $all_hosts
+do
+  all_hosts_count=$((all_hosts_count + 1))
+  for ip in $all_ip 
+  do
+    temp_result=$(dig $host|grep $ip)
+    if [[ $? == 0 ]]; then
+     echo "$host is resolved to $ip"
+     success=$((success + 1))
+    fi
+  done
+done
+
+echo "------------------------------ "
+echo "Tested VM count :  $all_hosts_count "
+echo "Success VM count : $success"
+if [[ $all_hosts_count == $success ]];then
+  echo "PASS !!"
+else
+  echo "FAIL ;("
+fi
