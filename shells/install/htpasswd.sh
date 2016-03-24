5d2c2b3b1f8b66f1611c54949a092c7c1b0573c1
root <root@master1.example.com>
2016-03-24 09:23:50 -0400

    htpasswd.sh

---
+++ shells/install/htpasswd.sh
@@ -0,0 +1,15 @@
+. ../ose_config.sh
+
+echo "Add user "joe" to OSE"
+echo "---------------------"
+echo ""
+for host in $all_hosts
+do
+  if [[ $host =~ ${master_prefix} ]]; then
+    htpasswd -b /etc/origin/master/htpasswd joe redhat
+    echo "joe user is added (pw:redhat)"
+    echo ""
+    systemctl restart atomic-openshift-master-api
+    echo "On $host, atomic-openshift-master-api restarted"
+  fi
+done
